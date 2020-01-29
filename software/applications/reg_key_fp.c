eb08eff6aa4cac49cc5a214a221896ac12d92330
ZHU Hao <hao_kr@163.com>
2020-01-29 12:08:17 +0800

    [keyobj] fp key update

--- software/applications/reg_key_fp.c
+++ software/applications/reg_key_fp.c
@@ -1,14 +1,11 @@
 #include "entry_key.h"
-#include "as608.h"
+#include "as608.h"  // as608 packages
 #include <math.h>
 
 #define AS60X_UART_NAME "uart3"
 
 static struct entry_key en_key_fp = {0};
-// static uint32_t 
 static uint8_t fp_id[KEY_ID_MAX] = {0};
-// static uint32_t fp_wr_flag = 0; 
-// static uint8_t best_wr_idx = 0;
 
 static void fp_add_key(uint8_t id)
 {
@@ -26,12 +23,13 @@ static void fp_del_key(uint8_t id)
     as60x_delet_fp_n_id(id, 1);
 }
 
-static uint8_t fp_ver_key(void)
+static uint16_t fp_ver_key(void)
 {
-
+    uint16_t id = as60x_search_fp_in_flash();
+    return ((id >= KEY_VER_ERROR) ? KEY_VER_ERROR : id);
 }
 
-static void rt_hw_fp_port(void)
+static int rt_hw_fp_port(void)
 {
     as60x_set_hand_shake_baud(115200);
     as60x_init(AS60X_UART_NAME);
@@ -41,6 +39,7 @@ static void rt_hw_fp_port(void)
     en_key_fp.ver_key = fp_ver_key;
     reg_key_obj(ENTRY_KEY_FP, &en_key_fp);
 
-    en_key_fp.enable = 1;
+    return 0;
 }
-INIT_COMPONENT_EXPORT(rt_hw_fp_port)
+// INIT_COMPONENT_EXPORT(rt_hw_fp_port);
+INIT_APP_EXPORT(rt_hw_fp_port);
